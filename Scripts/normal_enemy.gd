e9fef5209b0eb95b22553a0e953805c235b5e9bb
Kacz <zdavey@gmail.com>
2026-02-17 17:18:09 -0700

    Changed pathfinding to work with avoidance and the wind power to suit that

--- Scripts/normal_enemy.gd
+++ Scripts/normal_enemy.gd
@@ -3,7 +3,6 @@ class_name Enemy
 
 @onready var navigation_agent:NavigationAgent2D = $NavigationAgent2D
 @export var speed := 2000
-var external_velocity:Vector2
 
 func _ready() -> void:
 	var goal = get_tree().get_nodes_in_group("Goal")
@@ -17,14 +16,29 @@ func _physics_process(delta: float) -> void:
 	#if !navigation_agent.is_target_reachable():
 		#push_warning(self, " is lost (target not reachable)")
 		#return
-	
 	if !navigation_agent.is_target_reached():
-		var nav_point_direction = to_local(navigation_agent.get_next_path_position()).normalized()
-		apply_central_force(speed * nav_point_direction * delta + external_velocity)
+		var next_pos = navigation_agent.get_next_path_position()
+		var direction = (next_pos - global_position).normalized()
+		var velocity = direction * speed
+		# need to set the navigation agent velocity so it can calculate avoidance
+		# and use that instead
+		navigation_agent.velocity = velocity 
+		
 	else:
 		goal_reached()
 
+	#print(linear_velocity)	
+	#	var nav_point_direction = to_local(navigation_agent.get_next_path_position()).normalized()
+	#	apply_central_force(speed * nav_point_direction * delta)
+	#else:
+	#	goal_reached()
+
 
 func goal_reached():
+	print("goal reached")
 	pass
 	#kill enemy and remove health from player
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	apply_central_force(safe_velocity - linear_velocity)
--- Scripts/wind_controller.gd
+++ Scripts/wind_controller.gd
@@ -1,17 +1,20 @@
 extends Node
 
-func _process(delta):
-	if Input.is_key_pressed(KEY_ENTER):
-		print("Pressing Key")
-		global_wind()
+@export var wind_force = 1000
+var drag_direction = Vector2(0, 0)
 
-
-func global_wind():
+func global_wind(direction:Vector2):
 	print("Winnnnnd")
 	var targets = get_tree().get_nodes_in_group("affected by wind")
 	for target in targets:
-		target.parent_body.apply_force(Vector2(1000, 1000))
+		target.parent_body.apply_force(Vector2(direction.x * wind_force, direction.y * wind_force))
 
-# Find Direction
-
-# Apply force in that direction
+# Getting the mouse drags to know what direction to send wind.
+func _input(event: InputEvent) -> void:
+	# If we press wind button, start tracking the motion.
+	if Input.is_action_pressed("wind"):
+		if event is InputEventMouseMotion:
+				drag_direction = event.relative.normalized()
+	# Once we release the button act on that motion
+	if Input.is_action_just_released("wind"):
+		global_wind(drag_direction)
