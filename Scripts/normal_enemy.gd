4f4597dd26a1eaa05067092c9d19d98a3a856af4
Kacz <zdavey@gmail.com>
2026-02-18 16:17:12 -0700

    Enemies face correct direction when moving

--- Scripts/normal_enemy.gd
+++ Scripts/normal_enemy.gd
@@ -2,6 +2,7 @@ extends RigidBody2D
 class_name Enemy
 
 @onready var navigation_agent:NavigationAgent2D = $NavigationAgent2D
+@onready var sprite:Sprite2D = $Icon
 @export var speed := 2000
 
 func _ready() -> void:
@@ -13,6 +14,7 @@ func _ready() -> void:
 
 
 func _physics_process(delta: float) -> void:
+	
 	#if !navigation_agent.is_target_reachable():
 		#push_warning(self, " is lost (target not reachable)")
 		#return
@@ -27,17 +29,14 @@ func _physics_process(delta: float) -> void:
 	else:
 		goal_reached()
 
-	#print(linear_velocity)	
-	#	var nav_point_direction = to_local(navigation_agent.get_next_path_position()).normalized()
-	#	apply_central_force(speed * nav_point_direction * delta)
-	#else:
-	#	goal_reached()
-
-
 func goal_reached():
 	pass
 	#kill enemy and remove health from player
 
 
 func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	# Make the sprite look where it's going
+	var angle = sprite.global_position.angle_to_point(safe_velocity.normalized())
+	sprite.rotation = angle - PI/2
+	# Apply the force to steer
 	apply_central_force(safe_velocity - linear_velocity)
