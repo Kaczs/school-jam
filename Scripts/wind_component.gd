a7ff3cb99af6fe94c5268b553859c4ec0c8b3f07
Kacz <zdavey@gmail.com>
2026-02-18 23:31:04 -0700

    Spawn order tweaks

--- Scripts/wind_component.gd
+++ Scripts/wind_component.gd
@@ -8,7 +8,7 @@ var parent_body:RigidBody2D
 
 func _ready():
 	parent_body = get_parent() as RigidBody2D
-	if parent_body == null:
+	if parent_body == null:		
 		push_error("Parent of WindComponent needs to be a Rigid Body")
 	if toggled == true:
 		add_to_group("affected by wind")
@@ -19,9 +19,12 @@ func toggle():
 	toggled = not toggled
 	if toggled == false:
 		remove_from_group("affected by wind")
+		parent_body.freeze = true
 	if toggled == true:
 		add_to_group("affected by wind")
+		parent_body.freeze = false
 
 func _on_button_pressed() -> void:
+	# Freeze any momentum the object has, allows for cool slide cancel stuff
 	parent_body.linear_velocity = Vector2(0,0)
 	toggle()
