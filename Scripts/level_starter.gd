051b277d944c76a55c81344cd1f335381c20542d
Kacz <zdavey@gmail.com>
2026-02-19 02:05:46 -0700

    Losing is now in

--- Scripts/level_starter.gd
+++ Scripts/level_starter.gd
@@ -4,12 +4,16 @@ extends Node2D
 
 func _ready():
 	EventBus.connect("all_enemies_dead", win)
+	EventBus.connect("player_lose", lose)
 	var spawner:EnemySpawner = get_node_or_null("EnemySpawner")
 	if tutorial == false:
 		spawner.begin_spawns()
 
 func win():
-	print("you win")
 	LevelManager.level_complete(self)
+	var scene = load("res://MainMenu/main.tscn")
+	get_tree().change_scene_to_packed(scene)
+
+func lose():
 	var scene = load("res://MainMenu/main.tscn")
 	get_tree().change_scene_to_packed(scene)
